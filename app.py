9538a27fdfb6354cafec1e6ceb46aec8ec251ccb
bleepbloopsify <bleepbloopsify@gmail.com>
2015-12-17 13:06:55 -0500

    readme

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-#import utils
+from flask_socketio import SocketIO
+import utils
 
 app = Flask(__name__)
+app.secretkey = utils.secretkey
+
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
+    # if user not in session:
+    #     return redirect("/login")
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    if user in session:
+        return redirect("/home")
+    session['user'] = "here"
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.debug = True
