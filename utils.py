be34f9b23a458e9bcbf3ef69251d0a7bce994ae9
bleepbloopsify <bleepbloopsify@gmail.com>
2015-12-18 16:43:00 -0500

    pls

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 from pymongo import MongoClient
-from hashlib import md5
+import hashlib
 
 secretkey= "asdfghhjkl"
 
-def auth(username, password):    
+
+
+
+def auth(username, password):
    #Connect to the Mongodb
    connection = MongoClient()
    c = connection['data']
@@ -14,7 +17,7 @@ def auth(username, password):
    if not c.users.find_one({'username':username}):
        return False
    #Check the password
-   if not hashlib.md5(password) ==  c.users.find_one({'username':username})['password']:
+   if not hashlib.md5(password).hexdigest() ==  c.users.find_one({'username':username})['password']:
        return False
    #If Password and Uname match return true
    return True
@@ -25,27 +28,22 @@ def register(username, password, confirm_password):
     c = connection['data']
     #Check that the passwords are the same
     if not password == confirm_password:
-        return "Passwords do not match"
+        return False
     #Check that the username exists:
     if len(username) < 1:
-        return "Invalid Username"
+        return False
     #Check that the password exists:
     if len(password) < 1:
-        return "Invalid Password"
+        return False
     #Check if the username is taken
     if c.users.find_one({'username':username}) != None:
-        return "Username already taken"
+        return False
     #Encrypt Password
-    encrypted = hashlib.md5(password)
+    print password
+    encrypted = hashlib.md5(password).hexdigest()
     #Enter the information
     d = {'username': username,
          'password':encrypted,
          }
     c.users.insert(d)
-    return None
-def auth(username, password):
-    if username == "DM" and password == "DandD":
-        return True
-    else:
-        return False
-
+    return True
