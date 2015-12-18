f2e36a57ae7b1c30751ecb6830232984b8f1aa50
Kevin Yan <GuyWithTheGlasses@users.noreply.github.com>
2015-12-18 12:49:53 -0500

    Fixed merge

--- utils.py
+++ utils.py
@@ -2,14 +2,8 @@
 
 secretkey= "asdfghhjkl"
 
-<<<<<<< HEAD
 def auth(username, password):
     if username == "DM" and password == "DandD":
         return True
     else:
         return False
-
-=======
-def login(username, password):
-    return False
->>>>>>> Leon
