6532324dfa0f963a46df5bd7a7b17526d4453202
bleepbloopsify <bleepbloopsify@gmail.com>
2016-01-05 18:39:31 -0500

    POPUP YAY

--- static/Scripts/games.js
+++ static/Scripts/games.js
@@ -1,6 +1,34 @@
+var closewindow = function(){
+  $("#creategameform").css("visibility", "hidden");
+  console.log("unpls");
+};
+
+var openwindow = function(){
+  $("#creategameform").css("visibility", "visible");
+  console.log("pls");
+};
+
+
+// var sendgame = function(){
+//   $.ajax({
+//     url:"/creategame",
+//     method:"POST",
+//     data:{
+//       username:$("#username").val(),
+//       password:$("#password").val()
+//     },
+//     success: function(data){
+//       if (data == 'success'){
+//         $("#response").html("<success>Success!</success>");
+//         window.location.href = "/home"
+//       }else{
+//         $("#response").html("<err>Incorrect username or password.</err>");
+//       }
+//     }
+//   })
+// };
+
 $(document).ready(function(){
-  $("#creategame").click(function(){
-    $("#creategameform").css("visibility", "visible");
-    console.log('jel');
-  });
+  $("#creategame").click( openwindow );
+  $("#closegamewindow").click( closewindow );
 });
